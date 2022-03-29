2fa4ce6591cec1ad0af1190d09d23f978ddcb0e1
woaiziran <97371658+woaiziran@users.noreply.github.com>
2022-03-29 19:58:26 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -1,11 +1,18 @@
 #include<stdio.h>
-#include<math.h>
-#define r 1%
+#include<string.h>
+void encrypt(char* p,int num);//声明一个加密函数
 int main()
 {
-	double d=300000.0,p=6000.0;//贷款额d；每月还款额
-	double m;//还清月数
-	m = (log(p) - log(p - d * r)) / log(1 + r);
-	printf("需要%.2f还清",m);
+	char org[] = "China";//定义一个“China”的字符串
+	encrypt(org, strlen(org));//传入原码的指针及原码的长度
+	printf("%s\n", org);
 	return 0;
+}
+//声明一个加密函数
+void encrypt(char* p,int num)
+{
+	int i;
+	for (i = 0; i < num; i++) {//对原码进行加密，后移四位
+		p[i] = p[i] + 4;
+	}
 }
